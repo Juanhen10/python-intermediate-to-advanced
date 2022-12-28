9fd19043f5e157b3f5f37880b37e03896b4d6f8d
juan <henriquejuan36@gmail.com>
2022-12-28 17:50:21 -0300

    decorator

---
+++ decorantion/function_decorantion.py
@@ -0,0 +1,25 @@
+import os
+os.system('cls')
+# Funções decoradoras e decoradores
+# Decorar = adicionar / Remover / Restringir / Aleterar
+# Funçções decoradoras são funções que decoram outras funções
+# Decoradores são usadas para fazer o Python
+# usar as funções decoradoras em outras funções
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        for arg in args:
+            is_string(arg)
+        resultado = func(*args, **kwargs)
+        return resultado
+    return interna
+
+def inverte_string(string):
+    return string[::-1]
+
+def is_string(param):
+    if not isinstance(param,str):
+        raise TypeError('param deve ser uma string')
+
+inverte_string_checando_parametro = criar_funcao(inverte_string)
+invertida = inverte_string(123)
+print(invertida)
