8dd75531ee3619cb32ec62f0813a9ce9448a5ec9
juan <henriquejuan36@gmail.com>
2022-12-29 09:28:14 -0300

    decotator

--- decorantion/function_decorantion.py
+++ decorantion/function_decorantion.py
@@ -10,10 +10,13 @@ def criar_funcao(func):
         for arg in args:
             is_string(arg)
         resultado = func(*args, **kwargs)
+        print(f'o seu resultado foi {resultado}')
         print('ok, agora você foi decorada')
         return resultado
     return interna
 
+
+@criar_funcao
 def inverte_string(string):
     return string[::-1]
 
@@ -21,6 +24,6 @@ def is_string(param):
     if not isinstance(param,str):
         raise TypeError('param deve ser uma string')
 
-inverte_string_checando_parametro = criar_funcao(inverte_string)
-invertida = inverte_string_checando_parametro(123)
+
+invertida = inverte_string('123')
 print(invertida)
