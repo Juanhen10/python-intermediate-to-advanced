0189a09daa7254c912fd3fa6db4a5667e5499928
juan <henriquejuan36@gmail.com>
2023-01-06 11:06:20 -0300

    taks

--- challenge/task/task.py
+++ challenge/task/task.py
@@ -8,13 +8,30 @@ def linha(msg = None):
     else:
         print(f'-{msg}-')
         print('--'*15)
+
+
+def save(file, letra):
+    with open(file, letra, encoding='utf8') as file:
+        json.dump(lista, file, indent= 2)
+
+
+def ler(file, letra):
+    with open(file, letra,encoding='utf8') as file:
+        task = json.load(file)
+        for pos, item in enumerate(task):
+            print(f'{pos}------{item}')
+
+             
+      
+
+
     
 lista = []
 BASE_DIR = 'C:\\Users\\Juan.Henrique\\Desktop\\pythonn\\challenge\\task'
 JSON_FILE = os.path.join(BASE_DIR, 'list-task.json')
 
-with open(JSON_FILE, 'w', encoding='utf8') as file:
-    json.dump(lista, file, indent=2)
+# with open(JSON_FILE, 'w', encoding='utf8') as file:
+#     json.dump(lista, file, indent=2)
     
 while True:
     opc = int(input('ecolhas as opções: \n[1] - Criar tarefa \n[2]- Refazer lista\n[3]- Deletar algum item da lista\n[4]- ver lista\n[5] - Sair \nQual opc:  '))
@@ -23,6 +40,7 @@ while True:
         tarefas = str(input('Digite uma tarefa: '))
         lista.append(tarefas)
         print('tarefa criada!')
+        save(JSON_FILE, 'w')
         linha()
         continue
     elif opc == 2:
@@ -31,25 +49,18 @@ while True:
             lista.clear()
             print('Tarefas limpas')
             linha
+        save(JSON_FILE, 'w')
         if refaz == 'N':
             continue
     elif opc == 3:
-        try:
-            for pos, itens in enumerate(lista):
-                print(f'{pos}-----{itens}')
-            delete = int(input("Qual tarefa quer deletar: "))
-            if delete == pos:
-                lista.pop(pos)
-                print('item deletado!')
-                linha()
-        except:
-            print('não tem itens na lista')
-            linha()
-            continue
+        ler(JSON_FILE,'r')
+        deletar = input('delete uma tarefa: ')
+        for key, item in enumerate(lista):
+            lista.pop(key)
+        save(JSON_FILE,'w')
     elif opc == 4:
         linha('lista de tarefas')
-        for i in lista:
-            print(i)
+        ler(JSON_FILE,'r')
         linha()
     elif opc == 5:
         print('volte sempre!')
