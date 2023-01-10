ff356743158c2013d35e186fcedbdbd409abedbe
juan <henriquejuan36@gmail.com>
2023-01-10 16:06:39 -0300

    s

--- python poo/poo-exercices/json/save_classe.py
+++ python poo/poo-exercices/json/save_classe.py
@@ -1,6 +1,8 @@
 import json
 import os 
 os.system('cls')
+        
+CAMINHO = 'car.json'
 
 
 class Car:
@@ -20,36 +22,56 @@ class Car:
             print(f'CAR:{self.name} was solding!!')
         print(f'CAR:{self.name} was sold!!!!')
         self.solding = True
+        print('-'*30)
         
         
     def not_sold(self):
         if not self.solding:
             print(f"{self.name} isn't was solding")
+            print('-'*30)
+        
             return
         
         print(f'CAR: {self.name} was solding...')
         self.solding = True
+        print('-'*30)
         
     
         
    
         
-        
-CAMINHO = 'car.json'
 car_1 = Car('Golf GTI', '1998', 'GOLF', 'MK4')
 
 
-
+car_1.not_sold()
 
 car_2 = Car('Civic', '1998', 'HONDA')
+car_2.sold()
+
 car_3 = Car('Lancer', '2020', 'Mitsubishi', 'Mitsubishi Lancer HL')
+car_3.not_sold()
+
 car_4 = Car('A4', '2021', 'AUDI', 'Prestige')
-car_4 = Car('BMW M3', '2020', 'BMW', 'M4')
+car_4.sold()
+
+
+car_5 = Car('BMW M3', '2020', 'BMW', 'M4')
+car_5.sold()
+
+dados = [vars(car_1),vars(car_2),vars(car_3),vars(car_4),vars(car_5)]
 
 
 
-car_1.not_sold()
-car_1.sold()
-car_1.not_sold()
+def fazer_dump():
+    with open(CAMINHO, 'w', encoding='utf8') as file:
+        json.dump(dados, file, ensure_ascii=False, indent=2)
+        
+        
+if __name__ == '__main__':
+    print('#' * 40)
+    fazer_dump()
+          
+
+
 
 
