95fda9c8afc29e7d165df8b89cefb8c4876e9c02
juan <henriquejuan36@gmail.com>
2023-02-03 06:59:27 -0300

    class_decoration

---
+++ python poo/classe/classe_decoration.py
@@ -0,0 +1,21 @@
+import os 
+os.system('cls')
+# Classe decoradora (Decorator Classes)
+class Multiplicar:
+    def __init__(self, multiplicador):
+        self._multiplicador = multiplicador
+        
+        
+    def __call__(self, func):
+        def interna(*args, **kwargs):
+            resultado = func(*args, **kwargs)
+            return resultado *self._multiplicador
+        return interna
+        
+
+@Multiplicar(1)
+def soma(x, y):
+    return x + y
+
+soma = soma(2,4)
+print(soma)
