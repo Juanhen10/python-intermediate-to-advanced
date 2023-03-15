2b4b1255afbf51e046e26fc5b2ec9ec756b50505
juan <henriquejuan36@gmail.com>
2023-03-15 08:06:53 -0300

    Jupyer Notebook

---
+++ modules_in_python/jupyter_notebook/jp.py
@@ -0,0 +1,13 @@
+# Jupyter Notebook
+
+# Essa linha cria uma variável a
+a = 2
+
+# Essa linha tem o valor da varável 'a' em dobro
+b = a * 2  # 4
+
+# Essa linha mostra o valor que 'a + 'b'
+print(a + b)  # 6
+
+# comandos:
+# Jupyter nootebok para abrir o Note
