263183219ea4438d1a0f28a5138d77a42f1d7858
juan <henriquejuan36@gmail.com>
2023-04-03 05:29:35 -0300

    Pyside6

---
+++ PYside6 - interface Grafica/Project/main_window.py
@@ -0,0 +1,25 @@
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QVBoxLayout,
+                               QWidget)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+
+        # Configurando o layou básico
+        self.cw = QWidget()
+        self.v_layout = QVBoxLayout()
+        self.cw.setLayout(self.v_layout)
+        self.setCentralWidget(self.cw)
+
+        # Título da janela
+        self.setWindowTitle('Calculadora')
+
+    def adjustFixedSize(self):
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+
+    def addWidgetToVLayout(self, widget: QWidget):
+        self.v_layout.addWidget(widget)
+        self.adjustFixedSize()
