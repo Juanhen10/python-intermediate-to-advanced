202b7249c7d47ef65b947a4ff3f6b1f7aac9b082
juan <henriquejuan36@gmail.com>
2023-05-28 20:46:39 -0300

    SQL

--- Python_SQL/main.py
+++ Python_SQL/main.py
@@ -105,6 +105,7 @@ import dotenv
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: htpps://github.com/PyMySQL
 import pymysql
+import pymysql.cursors
 
 os.system('cls')
 TABLE_NAME = 'custormer'
@@ -116,10 +117,14 @@ connection = pymysql.Connect(
     user=os.environ['MYSQL_USER'],
     password=os.environ['MYSQL_PASSWORD'],
     database=os.environ['MYSQL_DATABASE'],
-    charset='utf8mb4'
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.SSDictCursor,
+    # cursorclass=pymysql.cursors.DictCursor,
 )
 
-# Inserindo dados
+########################################################################
+#                     ### CRIANDO VALORES ###                     C    #
+########################################################################
 with connection:
     with connection.cursor() as cursor:
         # SQL
@@ -127,14 +132,15 @@ with connection:
             f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
             'id INT NOT NULL AUTO_INCREMENT, '
             'name VARCHAR(50) NOT NULL, '
-            'years INT NOT NULL, '
+            'age INT NOT NULL, '
             'PRIMARY KEY (id) '
             ') '
         )
         # TRUNCATE: LIMPA A TABELA
         cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
     connection.commit()
-
+########################################################################
+#       Inserindo um valor usando placeholder e um iterável #
     with connection.cursor() as cursor:
         sql = (
             f'INSERT INTO {TABLE_NAME} '
@@ -147,7 +153,7 @@ with connection:
         # print(sql, data)
         # print(result)
     connection.commit()
-
+#####################################################################
     with connection.cursor() as cursor:
         sql = (
             f'INSERT INTO {TABLE_NAME} '
@@ -155,11 +161,110 @@ with connection:
             'VALUES '
             '(%(name)s, %(age)s) '
         )
-        data2 = {
-            "name": "Victor",
-            "age": 27,
-        }
+        data2 = (
+            {"name": "Victor", "age": 27, }
+        )
         result = cursor.execute(sql, data2)
         # print(sql, data)
         # print(result)
     connection.commit()
+#####################################################################
+#   Inserindo varios valores usando placeholder e uma tupla de dicionarios #
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(name, age) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data3 = (
+            {"name": "Leandri", "age": 28, },
+            {"name": "Julia", "age": 26, },
+            {"name": "Alisson", "age": 13, },
+        )
+        result = cursor.executemany(sql, data3)
+        # print(sql)
+        # print(data3)
+        # print(result)
+    connection.commit()
+#########################################################################    #
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(name, age) '
+            'VALUES '
+            '(%s, %s)'
+        )
+        data4 = (
+            ("Sulivan", 60, ),
+            ("Kovalsk", 50, ),
+        )
+        result = cursor.executemany(sql, data4)
+        # print(sql)
+        # print(data4)
+        # print(result)
+    connection.commit()
+########################################################################
+#                   ### LENDO VALORES COM SELECT ###               R    #
+########################################################################
+    with connection.cursor() as cursor:
+        # menor_id = int(input('Digite o menor id: '))
+        # maior_id = int(input('Digite o maior id: '))
+        menor_id = 1
+        maior_id = 2
+        sql = (
+            f'SELECT * FROM {TABLE_NAME} '
+            'WHERE id BETWEEN %s AND %s '
+        )
+        cursor.execute(sql, (menor_id, maior_id))
+        # print(cursor.mogrify(sql, (menor_id, maior_id)))
+
+        # data5 = cursor.fetchall()
+        # for row in data5:
+        #     print(row)
+########################################################################
+#                        ### UPDATE ###                           U    #
+########################################################################
+    with connection.cursor() as cursor:
+        sql = (
+            f'UPDATE {TABLE_NAME} '
+            'SET name=%s, age=%s '
+            'WHERE id = %s'
+        )
+
+        cursor.execute(sql, ('paulin', 15, 4))
+        connection.commit()
+
+        cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+
+        # data6 = cursor.fetchall_unbuffered()
+
+        for row in cursor.fetchall_unbuffered():
+            print(
+                f'\033[32mid:{row["id"]}\033[m - \033[31mName: {row["name"]} \033[m- \033[35mAge:{row["age"]}\033[m]')
+            if row['id'] >= 5:
+                break
+        print()
+        # cursor.scroll(1, 'absolute')
+        for row in cursor.fetchall_unbuffered():
+            print(row)
+
+        # for row in cursor.fetchall():
+        #     _id, name, age = row
+        #     print(_id, name, age)
+#######################################################################
+#                        ### DELETANDO ###                           D #
+########################################################################
+    with connection.cursor() as cursor:
+        sql = (
+            f'DELETE FROM {TABLE_NAME} '
+            'WHERE id = %s'
+        )
+
+        cursor.execute(sql, (2))
+        connection.commit()
+
+        cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+
+        # for row in cursor.fetchall():
+        # print(row)
