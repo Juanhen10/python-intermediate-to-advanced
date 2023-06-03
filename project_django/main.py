b9e3038720bd71c91f04710d4f88b75906cb8a25
juan <henriquejuan36@gmail.com>
2023-06-03 03:22:11 -0300

    Django

--- project_django/main.py
+++ project_django/main.py
@@ -1,2 +1,18 @@
 # django-admin startproject <nomeDoProjeto .> = inicia o projeto
+# django-admin --help = mostra os comandos
 # python manage.py runserver = subir o servidor
+
+
+# PASTA DO PROJETO FUNÇÕES
+'''
+manage.py = onde definimos o projeto (a raiz)
+
+(no arquivo do projeto)
+setting.py = configuração do projeto
+urls.py = urls de todos os programas, quais os subdominios 
+
+tipos do servidor:
+wsgi.py 
+asgi.py 
+
+'''
--- project_django/project/urls.py
+++ project_django/project/urls.py
@@ -15,8 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+#  HTTP Request <-> HTTP Response
+# MTV (MVC) - mode view tamplate
+
+
+def home(request):
+    print('home')
+    return HttpResponse('home')
+
+
+def blog(request):
+    return HttpResponse('Hello world')
+
+
 urlpatterns = [
+    path('', home),
+    path('blog/', blog),
     path('admin/', admin.site.urls),
 ]
